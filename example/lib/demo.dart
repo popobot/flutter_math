7e02947b2910b114614b77a697f170ee19989c63
Masanori Ichikawa <masanori.ichikawa@studyplus.jp>
2021-06-10 09:23:40 +0900

    linebreak demo

--- example/lib/demo.dart
+++ example/lib/demo.dart
@@ -3,8 +3,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_math/flutter_math.dart';
 import 'package:provider/provider.dart';
 
-import 'package:flutter_tex/flutter_tex.dart';
-
 class DemoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Center(
@@ -16,11 +14,6 @@ class DemoPage extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                // Text(
-                //   'Input your equation below',
-                //   style: Theme.of(context).textTheme.headline6,
-                // ),
-                // Divider(),
                 Padding(
                   padding: const EdgeInsets.all(10),
                   child: Consumer<TextEditingController>(
@@ -40,7 +33,7 @@ class DemoPage extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       // mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
                         Center(
@@ -51,18 +44,24 @@ class DemoPage extends StatelessWidget {
                         ),
                         Expanded(
                           child: Container(
+                            width: 272,
                             decoration: BoxDecoration(
                               border: Border.all(width: 1),
                               borderRadius: BorderRadius.circular(5.0),
                             ),
-                            alignment: Alignment.topCenter,
-                            padding: EdgeInsets.all(10),
+                            padding: EdgeInsets.all(16),
                             child: Consumer<TextEditingController>(
-                              builder: (context, controller, _) =>
-                                  SelectableMath.tex(
-                                controller.value.text,
-                                textStyle: TextStyle(fontSize: 22),
-                              ),
+                              builder: (context, controller, _) {
+                                final longEq = Math.tex(
+                                  controller.value.text,
+                                  textStyle:
+                                      Theme.of(context).textTheme.bodyText1,
+                                );
+                                final breakResult = longEq.texBreak();
+                                return Wrap(
+                                  children: breakResult.parts,
+                                );
+                              },
                             ),
                           ),
                         ),
@@ -70,49 +69,6 @@ class DemoPage extends StatelessWidget {
                     ),
                   ),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      // mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Center(
-                          child: Text(
-                            "Flutter TeX's output",
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(width: 1),
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                            padding: const EdgeInsets.all(10),
-                            child: Consumer<TextEditingController>(
-                              builder: (context, controller, _) => TeXView(
-                                // Must use a GlobalKey, otherwise it will stack
-                                key: GlobalKey(debugLabel: 'texView'),
-                                renderingEngine:
-                                    const TeXViewRenderingEngine.katex(),
-                                child: TeXViewDocument(
-                                  '\$\$${controller.value.text}\$\$',
-                                  id: '0',
-                                  style: TeXViewStyle(
-                                      backgroundColor: Colors.white),
-                                ),
-                                style:
-                                    TeXViewStyle(backgroundColor: Colors.white),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
               ],
             ),
           ),
